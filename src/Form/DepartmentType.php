2a65e228ccdb41b4ed6bcd03e9d34fc7cfe224e1
julien basquin <julienbasquin.jb@gmail.com>
2021-11-15 13:19:22 +0100

    mise en place api

--- src/Form/DepartmentType.php
+++ src/Form/DepartmentType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Department;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,12 @@ class DepartmentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Name')
-            ->add('Capacity')
+            ->add('Name', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('Capacity', NumberType::class, [
+                'label' => 'Capacité'
+            ])
         ;
     }
 
